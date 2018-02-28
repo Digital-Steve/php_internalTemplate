091ca47803c6794fa7640c9df48c026a4d40bedb
stevebeanblue <stevebeanblue@aol.com>
2018-02-28 21:34:33 +0000

    adjustments to navbar

--- views/master.php
+++ views/master.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include("head.php");?>
-    <body>
-      <!-- start of alpha/beta banner -->
-      <?php include("navbar.php");?>
-      <!-- content from other pages in loaded here -->
-      <div id="main" class="main-content">
-          <?php 
+  <?php include("head.php");?>
+  <body>
+    <!-- start of alpha/beta banner -->
+    <?php include("navBar.php");?>
+    <!-- content from other pages in loaded here -->
+    <div id="main" class="main-content">
+        <?php 
 
-            $whichPage = $_SESSION['whichPage'];
-            
-            // handle root call
-            if ($whichPage == ""){
-              $whichPage = 'home';
-            }
+          $whichPage = $_SESSION['whichPage'];
+          
+          // handle root call
+          if ($whichPage == ""){
+            $whichPage = 'home';
+          }
 
-            include($whichPage.".php");
+          include($whichPage.".php");
 
-          ?>
-      </div>
-      <!-- start of footer -->
-      <?php include("footer.php"); ?>
-      <!-- end of footer --> 
-    </body>
+        ?>
+    </div>
+    <!-- start of footer -->
+    <?php include("footer.php");?>
+    <!-- end of footer --> 
+  </body>
 </html>
