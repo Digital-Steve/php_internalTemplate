fdf4924787d774e6ad0ebc3b4f59d699546da169
stevebeanblue <stevebeanblue@aol.com>
2018-03-09 23:52:43 +0000

    amended post to save cloud function

---
+++ model/environmentVariables.php
@@ -0,0 +1,15 @@
+<?php
+
+    class envVars 
+    {
+        public $MYSQL_USER = "root";
+        public $MYSQL_PASSWORD = "root";
+        public $MYSQL_DSN = "dft-ddt-sb-stevenewman:europe-west1:sandbox-mysql";
+        public $MYSQL_DATABASE = "testProc";
+        public $CloudFunctionToken = "qp10xke84mfmfvmfodfdf0993388jsdkjskdj";
+        public $SaveCloudFunctionURL = "https://us-central1-dft-ddt-sb-stevenewman.cloudfunctions.net/";
+        public $SQLString = "";
+
+    }
+
+?>
--- model/objects/user.php
+++ model/objects/user.php
@@ -1,5 +1,6 @@
 <?php 
     include_once("../model/database.php");
+    include_once("../model/environmentVariables.php");
 
    class user 
    {
@@ -18,25 +19,40 @@
             
             $db->save($sqlString);
        }
-
+       //calls cloud function which writes to the db
        public function callSaveUser()
        {
+           // get variables such as database name ect from the app.yaml
+           $environmentVariables = new envVars();
+
+           // assign insert string
+           $environmentVariables->SQLString = "INSERT INTO user (username, email, isAdmin, rfd) VALUES ('" . $this->username . "'," . "'" . $this->useremail . "'," . $this->admin . ", 0 )";
+
+           $data_string = json_encode($environmentVariables);
+
+           //echo($data_string );
+           //exit;
+
            // create a new cURL resource
             $ch = curl_init();
 
             // set URL and other appropriate options
-            curl_setopt($ch, CURLOPT_URL, "https://us-central1-dft-ddt-sb-stevenewman.cloudfunctions.net/SaveProcUser");
+            curl_setopt($ch, CURLOPT_URL, $environmentVariables->SaveCloudFunctionURL . "SaveToMySQL");
             curl_setopt($ch, CURLOPT_HEADER, 0);
-
+            curl_setopt($ch, CURLOPT_HTTPHEADER,
+                    array("Content-type: application/json"));
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  
+            
             // grab URL and pass it to the browser
-            curl_exec($ch);
-
-            $response = json_decode(curl_exec($ch), true);
-
-            echo($response);
-
+            $response = curl_exec($ch);
+            
             // close cURL resource, and free up system resources
             curl_close($ch);
+
+            echo ($response);
+
        }
    }
 ?>
